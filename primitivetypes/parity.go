c23ddbb904da95a8dc2d18a3a51a8b4f6a5d9c8e
sunilffx <sunil.x.gopinath@gmail.com>
2018-01-26 15:23:22 +1100

    4.2: calculating parity, more efficient

--- primitivetypes/parity.go
+++ primitivetypes/parity.go
@@ -1,6 +1,8 @@
 package primitivetypes
 
-import "errors"
+import (
+	"errors"
+)
 
 // Parity returns 1 or 0 depending on if the number of 1s is even or odd
 func Parity(x int) (int, error) {
@@ -9,11 +11,8 @@ func Parity(x int) (int, error) {
 	}
 	var c int
 	for x != 0 {
-		c += (x & 1)
+		c ^= (x & 1)
 		x >>= 1
 	}
-	if c%2 == 0 {
-		return 0, nil
-	}
-	return 1, nil
+	return c, nil
 }
